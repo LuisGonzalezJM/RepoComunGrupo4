287940f52f944ba8708a0750591f50537046dafb
oscar <valcuevo58@gmail.com>
2025-02-17 13:29:51 +0100

    buenas tardes

--- FicheroBase1.java
+++ FicheroBase1.java
@@ -1,6 +1,7 @@
 //Este es el fichero original que viene creado
 //1.- Línea original de Luis
 //este comentario va de como sergio es bobo
+
 public class FicheroBase1{
     public static void main(String[] args) {
         System.out.println("FicheroBase1");
